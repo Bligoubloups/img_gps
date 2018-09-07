0628ea4053e739482d54e007703b46ad07f61ef3
Bligoubloups <marc2_leonetti@hotmail.com>
2018-09-07 16:37:33 +0200

    multiples files working

--- img_gps.php
+++ img_gps.php
@@ -8,28 +8,34 @@ function get_lat_long($str)
     return ((double)$values[0] + (double)$values[1]/60 + (double)$values[3]/(double)1000000/(double)3600);
 }
 
-if (file_exists($argv[1]) == 0)
+$i = 1;
+while ($i < $argc)
 {
-    echo "File not found...\n";
-    exit();
+    echo "==================================================================="."\n";
+    echo "File Name: ".$argv[$i]."\n";
+    $image = $argv[$i];
+    if (!file_exists($image))
+        echo "File not found...\n";
+    else
+    {
+        $result = shell_exec("identify -format '%[EXIF:*]' " . "'". $argv[$i] . "'" . " | grep \".*Latitude.*\|.*Longitude.*\"");
+        if (!$result)
+            echo "There is no GPS data :/ \n";
+        else
+        {
+            $result = explode("\n", $result);
+            $latitude = get_lat_long($result[0]);
+            $longitude = get_lat_long($result[2]);
+            if (preg_filter('/.*=([A-Z])/', '$1', $result[1]) == "S")
+                $latitude = -$latitude;
+            if (preg_filter('/.*=([A-Z])/', '$1', $result[3]) == "W")
+                $longitude = -$longitude;
+            echo "Latitude = " . $latitude . "\n";
+            echo "Longitude = " . $longitude . "\n";
+            echo "https://www.google.fr/maps/search/" . $latitude . "," . $longitude. "\n";
+        }
+    }
+    $i++;
 }
-
-$image = $argv[1];
-$result = shell_exec("identify -format '%[EXIF:*]' " . "'". $argv[1] . "'" . " | grep \".*Latitude.*\|.*Longitude.*\"");
-if (!$result)
-{
-    echo "There is no GPS data :/ \n";
-    exit();
-}
-$result = explode("\n", $result);
-$latitude = get_lat_long($result[0]);
-$longitude = get_lat_long($result[2]);
-if (preg_filter('/.*=([A-Z])/', '$1', $result[1]) == "S")
-    $latitude = -$latitude;
-if (preg_filter('/.*=([A-Z])/', '$1', $result[3]) == "W")
-    $longitude = -$longitude;
-echo "Latitude = " . $latitude . "\n";
-echo "Longitude = " . $longitude . "\n";
-echo "https://www.google.fr/maps/search/" . $latitude . "," . $longitude. "\n";
-
+echo "==================================================================="."\n";
 ?>
